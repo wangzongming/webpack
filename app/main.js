f3fd24491bb202724dd9c8e944e4b9bf05ecf262
孤独王 <1746809408@qq.com>
2017-12-11 14:11:06 +0800

    v0.02

--- app/main.js
+++ app/main.js
@@ -1,8 +1,14 @@
 //打包入口文件
+import './common/rem.js';//可伸缩rem 
+
+
 import { Wang } from './pages/Greeter';
 var ele = new Wang().createDom();
+var subtit = new Wang().createDom1();
+
 
 document.querySelector("#root").appendChild( ele );
+document.querySelector("#root").appendChild( subtit );
 
 
 
--- app/pages/Greeter.js
+++ app/pages/Greeter.js
@@ -1,12 +1,18 @@
 import config from '../config.json';
 import styles from '../style/main.less';
-// var logo = require('../img/logo.png');
 
 class Wang {
     createDom (text = 'hello world'){
         var div = document.createElement('div');
         div.className = styles.root;
         div.textContent = text;
+        return div;
+    }
+
+    createDom1 (){
+        var div = document.createElement('div');
+        div.className = styles.subtit;
+        div.innerHTML = '<a href="https://github.com/wangzongming"> by:m </a>';
         return div;
     }
 }
